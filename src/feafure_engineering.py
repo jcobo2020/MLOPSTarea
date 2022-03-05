3efe6ae104d581fa908fd2e3a72ef836f9dd5920
jcobo2020 <jcobo.ec@gmail.com>
2022-03-05 12:45:19 -0500

    creacion de archivos python basado en cuadernos

---
+++ src/feafure_engineering.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from datetime import datetime, date
+
+df = pd.read_csv('../data/input/dataset_credit_risk.csv')
+df = df.sort_values(by=["id", "loan_date"])
+df = df.reset_index(drop=True)
+df["loan_date"] = pd.to_datetime(df.loan_date)
+df_grouped = df.groupby("id")
+df["nb_previous_loans"] = df_grouped["loan_date"].rank(method="first") - 1
+df['avg_amount_loans_previous'] = (
+    df.groupby('id')['loan_amount'].apply(lambda x: x.shift().expanding().mean())
+)
+
+
+df['birthday'] = pd.to_datetime(df['birthday'], errors='coerce')
+df['age'] = (pd.to_datetime('today').normalize() - df['birthday']).dt.days // 365
+df['job_start_date'] = pd.to_datetime(df['job_start_date'], errors='coerce')
+df['years_on_the_job'] = (pd.to_datetime('today').normalize() - df['job_start_date']).dt.days // 365
+df['flag_own_car'] = df.flag_own_car.apply(lambda x : 0 if x == 'N' else 1)
+df = df[['id', 'age', 'years_on_the_job', 'nb_previous_loans', 'avg_amount_loans_previous', 'flag_own_car', 'status']]
+df.to_csv('../data/output/train_model.csv', index=False)
---
+++ src/training_model.py
@@ -0,0 +1,44 @@
+
+import pandas as pd
+
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    accuracy_score, confusion_matrix, recall_score, 
+    plot_confusion_matrix, precision_score, plot_roc_curve
+)
+
+from sklearn.ensemble import RandomForestClassifier
+from joblib import dump
+
+df = pd.read_csv('../data/output/train_model.csv')
+cust_df = df.copy()
+cust_df.fillna(0, inplace=True)
+Y = cust_df['status'].astype('int')
+
+cust_df.drop(['status'], axis=1, inplace=True)
+cust_df.drop(['id'], axis=1, inplace=True)
+
+X = cust_df
+X_train, X_test, y_train, y_test = train_test_split(X, Y, 
+                                                    stratify=Y, test_size=0.3,
+                                                    random_state = 123)
+# Using Synthetic Minority Over-Sampling Technique(SMOTE) to overcome sample imbalance problem.
+#Y = Y.astype('int')
+X_train, y_train = SMOTE().fit_resample(X_train, y_train)
+X_train = pd.DataFrame(X_train, columns=X.columns)
+model = RandomForestClassifier(n_estimators=5)
+
+model.fit(X_train, y_train)
+y_predict = model.predict(X_test)
+
+print('Accuracy Score is {:.5}'.format(accuracy_score(y_test, y_predict)))
+print('Precision Score is {:.5}'.format(precision_score(y_test, y_predict)))
+print('Recall Score is {:.5}'.format(precision_score(y_test, y_predict)))
+print(pd.DataFrame(confusion_matrix(y_test,y_predict)))
+
+plot_confusion_matrix(model, X_test, y_test) 
+plot_roc_curve(model, X_test, y_test)
+
+dump(model, '../train_models/model_risk.joblib') 
+
