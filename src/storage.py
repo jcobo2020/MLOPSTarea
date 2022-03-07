573720bb66baaf87bf459c4cd32dcbad6d2d4a10
jcobo2020 <jcobo.ec@gmail.com>
2022-03-07 18:06:21 -0500

    quitar comentarios

--- src/storage.py
+++ src/storage.py
@@ -9,16 +9,14 @@ STORAGEACCOUNTURL = os.getenv('STORAGE_ACCOUNT_URL')
 STORAGEACCOUNTKEY = os.getenv('STORAGE_ACCOUNT_KEY')
 CONTAINERNAME = os.getenv('CONTAINER_NAME')
 BLOBNAME = os.getenv('BLOB_NAME')
- 
+
 class Storage:
     def __init__(self):
         self.blob_service_client_instance = BlobServiceClient(
             account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
-        #self.blob_client_instance = self.blob_service_client_instance.get_blob_client(
-        #    CONTAINERNAME, BLOBNAME, snapshot=None)
+        
 
     def download(self,url,blobname):
-        #url = "../data/input/dataset_credit_risk.csv"
         blob_client_instance = self.blob_service_client_instance.get_blob_client(
             CONTAINERNAME, blobname, snapshot=None)
         with open(url, "wb") as my_blob:
@@ -32,4 +30,4 @@ class Storage:
         with open(url, "rb") as data:
             blob_client_instance.upload_blob(data,overwrite=True)
             pass
-        #subir los datos en output y el modelo modelo entrenado
+        
