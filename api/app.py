c5e1661fa541e2642195dcadd50c39dcfe9d0837
jcobo2020 <jcobo.ec@gmail.com>
2022-03-08 08:23:56 -0500

    cambio a get y mejora de singleton

--- api/app.py
+++ api/app.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_restful import Resource, Api
-import pandas as pd
-from joblib import  load
 import json
 import requests
 from singleton import singleton
@@ -14,27 +12,30 @@ api = Api(app)
 class CaracteristicasRiesgo(Resource):
     def get(self, user_id):
         
-        df = pd.read_csv('../data/output/train_model.csv')
+        df = singleton.model_train
         
         resultado = df[df.id==int(user_id)]
         resultado.sort_values('nb_previous_loans',ascending=False,inplace=True)
         #id,age,years_on_the_job,nb_previous_loans,avg_amount_loans_previous,flag_own_car,status
+        #devuelve un solo registro en base a nb_previous_loans el mayor valor 
         resultado=resultado.iloc[0,[3,4,1,2,5]].to_json()
         resultado = json.loads(resultado)
 
         return resultado
 
 class Prediccion(Resource):
-    def post(self, user_id):
-       
+    def get(self, user_id):
 
         url = 'http://127.0.0.1:5001/api/user/' + user_id + '/caracteristicasriesgo'
         r = requests.get(url)
         camposresult = r.json()
         
-        d = {str(user_id): [camposresult['nb_previous_loans'], camposresult['avg_amount_loans_previous'], camposresult['age'],camposresult['years_on_the_job'],camposresult['flag_own_car']]}
+        #armar el diccionario del user_id 
+        d = {str(user_id): [camposresult['nb_previous_loans'], 
+            camposresult['avg_amount_loans_previous'], camposresult['age'],camposresult['years_on_the_job'],
+            camposresult['flag_own_car']]}
         
-        resultado = singleton.reporter.predict([d[user_id]])
+        resultado = singleton.modelo.predict([d[user_id]])
         
         lists = resultado.tolist()
         json_str = json.dumps(lists)
